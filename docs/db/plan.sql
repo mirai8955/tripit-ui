27ed3c7a8c186e0600c1e7fc7cf231cc27397644
mirai8955 <mirai.neyanderu1212@gmail.com>
2025-07-13 15:02:52 +0900

    add column into plans table

--- docs/db/plan.sql
+++ docs/db/plan.sql
@@ -4,6 +4,12 @@
 CREATE TABLE `plan` (
     `id` BINARY(16) NOT NULL PRIMARY KEY,
     `user_id` BINARY(16) NOT NULL COMMENT "References users table",
+    `title` VARCHAR(100) NOT NULL,
+    `departure` VARCHAR(100) NOT NULL,
+    `destination` VARCHAR(100 NOT NULL,
+    `start_at` DATETIME DEFAULT CURRENT_DATETIME,
+    `end_at` DATETIME DEFAULT CURRENT_DATETIME,
+    `duration` tinyint NOT NULL,
 
 )ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
